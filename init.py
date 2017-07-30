127ad75d83cde5cf72586d598b35aee7b52ffa83
kotland <dasha0105kot@gmail.com>
2017-07-30 14:17:35 +0700

    fix errors, add tie check, improve game logic

--- init.py
+++ init.py
@@ -7,31 +7,40 @@ field = [
     [" ", " ", " "],
 ]
 
-def check_input():
+def check_input(position):
+    try:
+        val = int(position)
+        if (val < 10 and val > 0):
+            return 0
+    except:
+        return -1
+    return -1
+
+def user_turn(field):
+    print("user_turn:", end=' ')
     while (True):
         position = input()
-        try:
-            if (int(position) < 10):
-                break
-        except:
+        if(check_input(position) < 0):
             continue
-    return position
+        if(user_input(int(position), field) < 0):
+            continue
+        break
 
-def inputs():
-    position = int(check_input())
+def user_input(position, field):
     row = position / 3
     row = math.ceil(row)
-    position = 2 - (row * 3 - position)
+    position = position - row * 3 - 1
     if (field[row - 1][position] == " "):
         field[row - 1][position] = "X"
+        return 0
     else:
-        inputs()
+        return -1
 
-def print_field():
+def print_field(field):
     for row in field:
         print(row)
 
-def computers():
+def computer_turn(field):
     while(True):
         row = random.randint(0, 2)
         pos = random.randint(0, 2)
@@ -41,30 +50,47 @@ def computers():
         else:
             continue
 
-def check_win():
-    for row in range(0, 2):
+def check_win(field):
+    for row in range(0, 3):
         if (field[row][0] == field[row][1] == field[row][2] != " "):
-            print(field[row][0].__str__() + " win!")
-            exit()
+            return field[row][0].__str__() + " win!"
 
-    for cel in range(0, 2):
-        if (field[0][cel] == field[1][cel] == field[2][cel] != " "):
-            print(field[row][0].__str__() + " win!")
-            exit()
+    for col in range(0, 3):
+        if (field[0][col] == field[1][col] == field[2][col] != " "):
+            return field[0][col].__str__() + " win!"
 
     if (field[0][0] == field[1][1] == field[2][2] != " "):
-        print(field[row][0].__str__() + " win!")
-        exit()
+        return field[0][0].__str__() + " win!"
 
     if (field[0][2] == field[1][1] == field[2][0] != " "):
-        print(field[row][0].__str__() + " win!")
-        exit()
+        return field[0][2].__str__() + " win!"
+
+    full = True
+    for row in field:
+        for cell in row:
+            if (cell == " "): full = False
+    if(full): return "Tie game!"
+
+    return ""
+
+def main():
+    print("Welcome to Tic-Tac-Toe")
+    game_field = field
+    while(True):
+        user_turn(game_field)
+        result = check_win(game_field)
+        if (result != ""):
+            print_field(game_field)
+            print(result)
+            break
+        computer_turn(game_field)
+        result = check_win(game_field)
+        print_field(game_field)
+        if (result != ""):
+            print(result)
+            break
+        else:
+            print("Wrong input")
 
 
-
-while(True):
-
-    inputs()
-    computers()
-    print_field()
-    check_win()
+main()
