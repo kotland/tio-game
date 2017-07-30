e1a04e8de33a3d92736b781a1c85a4ff2384210e
kotland <dasha0105kot@gmail.com>
2017-07-30 14:52:04 +0700

    Add testing

--- init.py
+++ init.py
@@ -6,7 +6,62 @@ field = [
     [" ", " ", " "],
     [" ", " ", " "],
 ]
+#test_fields[Number of cases][0 - field \ 1 - result]
+test_fields = [
+[[["X", "O", " "],
+  [" ", "O", "X"],
+  [" ", "O", "X"]], "O win!"],
 
+[[[" ", "O", " "],
+  ["X", "X", "X"],
+  [" ", "O", " "]], "X win!"],
+
+[[["O", " ", "X"],
+  ["O", "X", " "],
+  ["O", " ", "X"]], "O win!"],
+
+[[[" ", " ", "O"],
+  [" ", "O", " "],
+  ["X", "X", "X"]], "X win!"],
+
+[[["X", "O", " "],
+  [" ", "X", " "],
+  [" ", "O", "X"]], "X win!"],
+
+[[["X", "X", "O"],
+  ["X", "O", " "],
+  ["O", " ", " "]], "O win!"]
+]
+
+test_fail_fields = [
+[[["X", "O", " "],
+  [" ", "O", "X"],
+  [" ", "O", "X"]], "X win!"],
+
+[[["O", "X", "O"],
+  ["O", "X", "X"],
+  ["X", "O", "O"]], "X win!"],
+
+[[["O", " ", "X"],
+  ["O", "X", " "],
+  ["O", " ", "X"]], "X win!"],
+
+[[[" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "]], "X win!"],
+
+[[[" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "]], "O win!"],
+
+[[["X", "O", "O"],
+  ["O", "X", "X"],
+  ["X", "O", "X"]], "Tie game!"],
+
+[[["X", "X", "O"],
+  ["X", "O", " "],
+  ["O", " ", " "]], ""]
+]
 def check_input(position):
     try:
         val = int(position)
@@ -73,24 +128,54 @@ def check_win(field):
 
     return ""
 
+def test():
+    check_win_complete = True
+    print("Testing check_win on positive cases")
+    for case in test_fields:
+        print_test(case)
+        if (case[1] != check_win(case[0])):
+            check_win_complete = False
+            print("Wrong!")
+    print("Testing check_win on negative cases")
+    for case in test_fail_fields:
+        print_test(case)
+        if (case[1] == check_win(case[0])):
+            check_win_complete = False
+            print("Wrong!")
+
+    if (check_win_complete): print("check_win:\t[OK]") 
+    else: print("check_win:\t[FAIL]")
+
+def print_test(test_field):
+    print_field(test_field[0])
+    print(test_field[1], "? ", "True" if test_field[1] == check_win(test_field[0]) else "False")
+
 def main():
     print("Welcome to Tic-Tac-Toe")
-    game_field = field
-    while(True):
-        user_turn(game_field)
-        result = check_win(game_field)
-        if (result != ""):
-            print_field(game_field)
-            print(result)
-            break
-        computer_turn(game_field)
-        result = check_win(game_field)
-        print_field(game_field)
-        if (result != ""):
-            print(result)
-            break
+    print("Input 1 to test, 2 to play game")
+    var = input()
+    try:
+        var = int(var)
+        if (var == 1):
+            test()
+        elif (var == 2):
+            game_field = field
+            while(True):
+                user_turn(game_field)
+                result = check_win(game_field)
+                if (result != ""):
+                    print_field(game_field)
+                    print(result)
+                    break
+                computer_turn(game_field)
+                result = check_win(game_field)
+                print_field(game_field)
+                if (result != ""):
+                    print(result)
+                    break
         else:
             print("Wrong input")
-
+    except:
+        print("Wrong input")
 
 main()
